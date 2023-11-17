4586b079c920066ebb9b102a2fd0f7f94b24fc59
dongju <ldj5026@naver.com>
2023-11-17 16:40:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/aaaaaaa/MainActivity2.java
@@ -0,0 +1,12 @@
+package com.example.aaaaaaa;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity2 extends MainActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
